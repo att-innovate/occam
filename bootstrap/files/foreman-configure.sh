5c4374474fbc23fd9049b0be142ec63814c537c6
James Kyle <james@jameskyle.org>
2014-10-28 11:57:42 -0700

    Added automatic discovery and operating system templates

--- bootstrap/files/foreman-configure.sh
+++ bootstrap/files/foreman-configure.sh
@@ -17,10 +17,28 @@ function subnet() {
   fi
 }
 
+function templates() {
+  ${HAMMER} template update \
+      --name "PXELinux global default" \
+      --file /tmp/pxelinux_global.erb
+  
+  arch_id=`${HAMMER} architecture list | awk '/x86_64/{print $1}'`
+  medium_id=`${HAMMER} medium list | awk '/Ubuntu mirror/{print $1}'`
+  ptable_id=`${HAMMER} partition-table list | awk '/Preseed custom LVM/{print $1}'`
+  hammer os create \
+    --architecture-ids ${arch_id} \
+    --family Debian \
+    --major 12 \
+    --minor 04 \
+    --medium-ids ${medium_id} \
+    --name "Ubuntu" \
+    --ptable-ids ${ptable_id}
+}
+
 function main() {
-  #environment testing
-  ${HAMMER} environment create --name testing
-  ${HAMMER} environment create --name develop
+  environment testing
+  environment develop
+  templates
   touch /etc/foreman/configure_success.txt
 }
 
