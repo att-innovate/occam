786d3002a4ee2919a0e1944e3edf659804f15196
James Kyle <james@jameskyle.org>
2015-01-23 12:18:45 -0800

    Improved validation.

--- tasks.py
+++ tasks.py
@@ -22,14 +22,18 @@
 ##                                                                           ##
 ###############################################################################
 import os
+import sys
+import vmfusion
+
 from invoke import Collection, run, task
-from invoke.exceptions import Failure
 from distutils.version import LooseVersion
+from colors import red, green
 
 VIRTUALBOX_VERSION = LooseVersion('4.3')
 VMWARE_VERSION = LooseVersion('6.0')
 VAGRANT_VERSION = LooseVersion('1.7.2')
 VMWARE_NETWORK_FILE = "/Library/Preferences/VMware Fusion/networking"
+DEMO_SUBNET = "192.168.100.0"
 
 @task
 def build_docs():
@@ -47,48 +51,107 @@ def clean_docs():
     run("make clean")
     os.chdir(old_cwd)
 
-@task
-def validate(provider="vmware"):
-    """Validate the working environment."""
+def validate_vagrant():
+    sys.stdout.write(green("Vagrant is found in path..."))
+    try:
+        run("which vagrant", hide=True)
+        print(green("OK"))
+    except:
+        print(red("FAIL"))
+
+    msg = green("Vagrant version is {0} or greater...".format(VAGRANT_VERSION))
+    sys.stdout.write(msg)
 
     try:
         result = run("vagrant --version 2>&1", hide=True)
         version = LooseVersion(result.stdout.split()[1].rstrip())
-        if VAGRANT_VERSION > version:
-            msg = "Detected vagrant %s, suggested %s or greater"
-            print(msg % (version, VMWARE_VERSION))
-        else:
-            print("Found Vagrant version %s....OK" % version)
-    except Failure as e:
-        print("Vagrant failed with error: %s" % e.result.stdout)
 
+        if VAGRANT_VERSION > version:
+            print(red("FAIL"))
+        else:
+            print(green("OK"))
+    except:
+        msg = "WARNING:: Vagrant command failed with error, please verify the "
+        msg += "vagrant install."
+        print(red(msg))
+
+def validate_vmware():
+    path = "/Applications/VMware\ Fusion.app/Contents"
+    cmd = "defaults read %s/Info.plist CFBundleShortVersionString" % path
+
+    # find a validly configured vmware custom network
+    found = False
+    for num in range(1,9):
+        try:
+            net = vmfusion.vnet_cli("vmnet{0}".format(num))
+            if net.subnet == DEMO_SUBNET:
+                found = True
+                break
+        except ValueError:
+            # network is not defined
+            pass
+
+    sys.stdout.write(green("Found custom demo net {0}...".format(DEMO_SUBNET)))
+
+    if found:
+        print(green("OK"))
+        sys.stdout.write(green("Custom net DHCP is disabled..."))
+        if net.dhcp:
+            print(red("FAIL"))
+        else:
+            print(green("OK"))
+    else:
+        print(red("FAIL"))
+
+    msg = "Checking VMware version {0} or greater...".format(VMWARE_VERSION)
+    sys.stdout.write(green(msg))
+
+    try:
+        result = run(cmd, hide=True)
+        version = LooseVersion(result.stdout.rstrip())
+    except:
+        print(red("\nWARNING:: Could not determine VMware version!"))
+        version = LooseVersion("0.0")
+
+    if VMWARE_VERSION > version:
+        print(red("FAIL"))
+    else:
+        print(green("OK"))
+
+
+def validate_virtualbox():
+    try:
+        result = run("VBoxManage --version 2>&1", hide=True)
+        version = LooseVersion(result.stdout.rstrip())
+        if VIRTUALBOX_VERSION > version:
+            msg = "Detected virtualbox %s, sugggested %s or greater."
+            print(msg % (version, VIRTUALBOX_VERSION ))
+        else:
+            print(green("Found Virtualbox version {0}....OK".format(version)))
+    except:
+        print(red("Could not find VBoxManage command!"))
+
+
+def rvm_warning():
+    path = os.path.join(os.path.expanduser("~"), ".rvm")
+    if os.path.exists(path):
+        msg = "WARNING:: RVM detected. RVM does magical things to your shell.\n"
+        msg += "WARNING:: Helper scripts may not work if vagrant is installed\n"
+        msg += "          via RVM. Please consider a standard vagrant install.\n"
+        msg += "WARNING:: Vagrant installer can be downloaded at \n"
+        msg += "          https://www.vagrantup.com/downloads.html"
+        print(red(msg))
+
+@task
+def validate(provider="vmware"):
+    """Validate the working environment."""
+    rvm_warning()
+    validate_vagrant()
 
     if provider == "virtualbox":
-        try:
-            result = run("VBoxManage --version 2>&1", hide=True)
-            version = LooseVersion(result.stdout.rstrip())
-            if VIRTUALBOX_VERSION > version:
-                msg = "Detected virtualbox %s, sugggested %s or greater."
-                print(msg % (version, VIRTUALBOX_VERSION ))
-            else:
-                print("Found Virtualbox version %s....OK" % version)
-        except:
-            print("Could not find VBoxManage command!")
-
-
+        validate_virtualbox()
     elif provider == "vmware":
-        path = "/Applications/VMware\ Fusion.app/Contents"
-        cmd = "defaults read %s/Info.plist CFBundleShortVersionString" % path
-        try:
-            result = run(cmd, hide=True)
-            version = LooseVersion(result.stdout.rstrip())
-        except:
-            print("Could not determine VMware version!")
-        if VMWARE_VERSION > version:
-            msg = "Detected vmware %s, suggested %s or greater."
-            print(msg % (version, VMWARE_VERSION))
-        else:
-            print("Found VMware version %s....OK" % version)
+        validate_vmware()
 
 
 @task(validate)
